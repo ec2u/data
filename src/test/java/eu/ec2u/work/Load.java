b21a536a29c3c83c5c15757fd0205df1c7d7a35f
Alessandro Bollini <alessandro.bollini@metreeca.com>
2021-02-24 20:21:21 +0100

    Load namespaces from ontologies

--- src/test/java/eu/ec2u/work/Load.java
+++ src/test/java/eu/ec2u/work/Load.java
@@ -8,17 +8,28 @@ import com.metreeca.json.Frame;
 import com.metreeca.open.actions.WikidataMirror;
 import com.metreeca.rdf.actions.Retrieve;
 import com.metreeca.rdf4j.actions.Upload;
+import com.metreeca.rdf4j.assets.Graph;
 import com.metreeca.rest.Xtream;
+import com.metreeca.rest.assets.Logger;
 
+import eu.ec2u.data.Data;
 import eu.ec2u.data.schemas.EC2U;
-import eu.ec2u.data.schemas.EWP;
-import org.eclipse.rdf4j.model.vocabulary.*;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.Rio;
+import org.eclipse.rdf4j.rio.helpers.AbstractRDFHandler;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static com.metreeca.json.Values.iri;
 import static com.metreeca.rdf4j.assets.Graph.graph;
 import static com.metreeca.rest.Context.asset;
 import static com.metreeca.rest.Context.resource;
+import static com.metreeca.rest.assets.Logger.logger;
 
 import static eu.ec2u.work.Work.exec;
 
@@ -26,20 +37,54 @@ import static eu.ec2u.work.Work.exec;
 final class Load {
 
 	@Test void namespaces() {
-		exec(() -> asset(graph()).exec(connection -> {
+		exec(new Runnable() {
 
-			connection.setNamespace("ec2u", EC2U.Name);
-			connection.setNamespace("ewp", EWP.Name);
+			private final Graph graph=asset(graph());
+			private final Logger logger=asset(logger());
 
-			connection.setNamespace("rdf", RDF.NAMESPACE);
-			connection.setNamespace("rdfs", RDFS.NAMESPACE);
-			connection.setNamespace("xsd", XSD.NAMESPACE);
-			connection.setNamespace("owl", OWL.NAMESPACE);
-			connection.setNamespace("skos", SKOS.NAMESPACE);
-			connection.setNamespace("void", VOID.NAMESPACE);
+			@Override public void run() {
+				graph.exec(connection -> {
+					try {
 
-		}));
+						connection.clearNamespaces();
+
+						Files.walk(Paths.get(resource(Data.class, "schemas").toURI()))
+								.filter(path -> path.toString().endsWith(".ttl"))
+								.forEach(path -> {
+									try {
+
+										Rio.createParser(RDFFormat.TURTLE).setRDFHandler(new AbstractRDFHandler() {
+
+											@Override public void handleNamespace(final String prefix,
+													final String uri) {
+
+												logger.info(this, String.format("@prefix %s: <%s>", prefix, uri));
+												connection.setNamespace(prefix, uri);
+											}
+
+										}).parse(Files.newInputStream(path));
+
+									} catch ( final IOException e ) {
+
+										throw new UncheckedIOException(e);
+
+									}
+								});
+
+					} catch ( final URISyntaxException ignored ) {
+
+
+					} catch ( final IOException e ) {
+
+						throw new UncheckedIOException(e);
+
+					}
+
+				});
+			}
+		});
 	}
+
 
 	@Test void universities() {
 		exec(() -> Xtream
