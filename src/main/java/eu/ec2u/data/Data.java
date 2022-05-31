4a9b1f624e9c6af624e323735e28bb813cfc44da
Alessandro Bollini <alessandro.bollini@metreeca.com>
2022-05-31 13:08:47 +0200

    Lay down Cloud Run Deployment

--- src/main/java/eu/ec2u/data/Data.java
+++ src/main/java/eu/ec2u/data/Data.java
@@ -17,43 +17,25 @@
 package eu.ec2u.data;
 
 import com.metreeca.gcp.GCPServer;
-import com.metreeca.gcp.services.*;
 import com.metreeca.http.Locator;
-import com.metreeca.http.Request;
 import com.metreeca.http.handlers.*;
 import com.metreeca.http.services.Cache.FileCache;
 import com.metreeca.http.services.Fetcher.CacheFetcher;
 import com.metreeca.http.services.Fetcher.URLFetcher;
-import com.metreeca.rdf4j.handlers.Graphs;
-import com.metreeca.rdf4j.handlers.SPARQL;
-import com.metreeca.rdf4j.services.Graph;
-import com.metreeca.rdf4j.services.GraphEngine;
+import com.metreeca.jse.JSEServer;
 
-import eu.ec2u.data.ports.Concepts;
-import eu.ec2u.data.ports.*;
-import eu.ec2u.data.tasks.*;
 import eu.ec2u.data.terms.EC2U;
-import org.eclipse.rdf4j.repository.Repository;
-import org.eclipse.rdf4j.repository.sparql.SPARQLRepository;
 
-import java.net.URI;
 import java.nio.file.Paths;
-import java.util.Map;
 
 import static com.metreeca.http.Handler.handler;
-import static com.metreeca.http.Locator.service;
 import static com.metreeca.http.Locator.storage;
-import static com.metreeca.http.Response.SeeOther;
+import static com.metreeca.http.Response.OK;
 import static com.metreeca.http.services.Cache.cache;
 import static com.metreeca.http.services.Fetcher.fetcher;
 import static com.metreeca.http.services.Logger.Level.debug;
-import static com.metreeca.http.services.Store.store;
-import static com.metreeca.http.services.Vault.vault;
 import static com.metreeca.jsonld.codecs.JSONLD.keywords;
-import static com.metreeca.jsonld.services.Engine.engine;
-import static com.metreeca.rdf4j.services.Graph.graph;
 
-import static java.lang.String.format;
 import static java.time.Duration.ofDays;
 
 public final class Data implements Runnable {
@@ -61,8 +43,8 @@ public final class Data implements Runnable {
     private static final boolean Production=GCPServer.production();
 
 
-    private static final String RootRole="root"; // !!! remove
-    private static final String RootKey="root-key"; // !!! remove
+    //private static final String RootRole="root"; // !!! remove
+    //private static final String RootKey="root-key"; // !!! remove
 
 
     static {
@@ -75,41 +57,41 @@ public final class Data implements Runnable {
     public static Locator services(final Locator locator) {
         return locator
 
-                .set(vault(), GCPVault::new)
-                .set(store(), GCPStore::new)
+                //.set(vault(), GCPVault::new)
+                //.set(store(), GCPStore::new)
 
                 .set(storage(), () -> Paths.get(Production ? "/tmp" : "data"))
                 .set(fetcher(), () -> Production ? new URLFetcher() : new CacheFetcher())
                 .set(cache(), () -> new FileCache().ttl(ofDays(1)))
 
-                .set(graph(), () -> new Graph(repository()))
-                .set(engine(), GraphEngine::new)
+                //.set(graph(), () -> new Graph(repository()))
+                //.set(engine(), GraphEngine::new)
 
                 .set(keywords(), () -> EC2U.Keywords);
     }
 
 
-    private static String token() {
-        return service(vault()).get(RootKey).orElseThrow(() ->
-                new IllegalStateException(format("undefined secret <%s>", RootKey))
-        );
-    }
+    //private static String token() {
+    //    return service(vault()).get(RootKey).orElseThrow(() ->
+    //            new IllegalStateException(format("undefined secret <%s>", RootKey))
+    //    );
+    //}
 
-    private static Repository repository() {
-        if ( Production ) {
-
-            return new GCPRepository("graph");
-
-        } else {
-
-            final SPARQLRepository repository=new SPARQLRepository(EC2U.item("/sparql").stringValue());
-
-            repository.setAdditionalHttpHeaders(Map.of("Authorization", format("Bearer %s", token())));
-
-            return repository;
-
-        }
-    }
+    //private static Repository repository() {
+    //    if ( Production ) {
+    //
+    //        return new GCPRepository("graph");
+    //
+    //    } else {
+    //
+    //        final SPARQLRepository repository=new SPARQLRepository(EC2U.item("/sparql").stringValue());
+    //
+    //        repository.setAdditionalHttpHeaders(Map.of("Authorization", format("Bearer %s", token())));
+    //
+    //        return repository;
+    //
+    //    }
+    //}
 
 
     public static void main(final String... args) {
@@ -120,16 +102,11 @@ public final class Data implements Runnable {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     @Override public void run() {
-        new GCPServer().delegate(locator -> services(locator)
-
-                .exec(new Namespaces())
-                .exec(new Ontologies())
-                .exec(new Inferences())
+        new JSEServer().delegate(locator -> services(locator)
 
                 .get(() -> handler(
 
                         new CORS(),
-                        new Bearer(token(), RootRole), // !!! remove
 
                         new Wrapper()
 
@@ -140,31 +117,9 @@ public final class Data implements Runnable {
 
                         new Router()
 
-                                .path("/graphs", new Graphs()
-                                        .query()
-                                        .update(RootRole) // !!! remove
-                                )
+                                .path("/", new Router()
 
-                                .path("/sparql", handler(Request::route,
-
-                                        (request, forward) -> request.reply(SeeOther, URI.create(format(
-                                                "https://apps.metreeca.com/self/#endpoint=%s", request.item()
-                                        ))),
-
-                                        new SPARQL()
-                                                .query()
-                                                .update(RootRole) // !!! remove
-
-                                ))
-
-                                .path("/cron/*", new Cron())
-
-                                .path("/*", new Router()
-
-                                        .path("/", new Resources())
-                                        .path("/concepts/*", new Concepts())
-                                        .path("/universities/*", new Universities())
-                                        .path("/events/*", new Events())
+                                        .get((request, forward) -> request.reply(OK, "ciao!"))
 
                                 )
 
@@ -172,5 +127,60 @@ public final class Data implements Runnable {
 
         ).start();
     }
+
+
+    //@Override public void run() {
+    //    new GCPServer().delegate(locator -> services(locator)
+    //
+    //            .exec(new Namespaces())
+    //            .exec(new Ontologies())
+    //            .exec(new Inferences())
+    //
+    //            .get(() -> handler(
+    //
+    //                    new CORS(),
+    //                    new Bearer(token(), RootRole), // !!! remove
+    //
+    //                    new Wrapper()
+    //
+    //                            .before(request -> request
+    //                                    .base(EC2U.Base) // define canonical base
+    //                                    .header("Accept-Language", "") // disable language negotiation
+    //                            ),
+    //
+    //                    new Router()
+    //
+    //                            .path("/graphs", new Graphs()
+    //                                    .query()
+    //                                    .update(RootRole) // !!! remove
+    //                            )
+    //
+    //                            .path("/sparql", handler(Request::route,
+    //
+    //                                    (request, forward) -> request.reply(SeeOther, URI.create(format(
+    //                                            "https://apps.metreeca.com/self/#endpoint=%s", request.item()
+    //                                    ))),
+    //
+    //                                    new SPARQL()
+    //                                            .query()
+    //                                            .update(RootRole) // !!! remove
+    //
+    //                            ))
+    //
+    //                            .path("/cron/*", new Cron())
+    //
+    //                            .path("/*", new Router()
+    //
+    //                                    .path("/", new Resources())
+    //                                    .path("/concepts/*", new Concepts())
+    //                                    .path("/universities/*", new Universities())
+    //                                    .path("/events/*", new Events())
+    //
+    //                            )
+    //
+    //            ))
+    //
+    //    ).start();
+    //}
 
 }
