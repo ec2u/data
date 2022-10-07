930876e8ac3e53911489222f5acfd444ef19d02a
Alessandro Bollini <alessandro.bollini@metreeca.com>
2022-10-07 16:28:04 +0200

    Ontologies / Ignore immaterial localizations

--- src/main/java/eu/ec2u/data/tasks/Ontologies.java
+++ src/main/java/eu/ec2u/data/tasks/Ontologies.java
@@ -4,6 +4,7 @@
 
 package eu.ec2u.data.tasks;
 
+import com.metreeca.link.Values;
 import com.metreeca.rdf4j.actions.Upload;
 
 import eu.ec2u.data.terms.EC2U;
@@ -18,36 +19,44 @@ import static eu.ec2u.data.tasks.Tasks.ontologies;
 
 public final class Ontologies implements Runnable {
 
-	public static void main(final String... args) {
-		exec(() -> new Ontologies().run());
-	}
+    public static void main(final String... args) {
+        exec(() -> new Ontologies().run());
+    }
 
 
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	@Override public void run() {
-		ontologies()
+    @Override public void run() {
+        ontologies()
 
-				.flatMap(url -> {
+                .flatMap(url -> {
 
-					try ( final InputStream input=url.openStream() ) {
+                    try ( final InputStream input=url.openStream() ) {
 
-						return Rio.parse(input, RDFFormat.TURTLE).stream();
+                        return Rio.parse(input, RDFFormat.TURTLE).stream();
 
-					} catch ( final IOException e ) {
+                    } catch ( final IOException e ) {
 
-						throw new UncheckedIOException(e);
+                        throw new UncheckedIOException(e);
 
-					}
+                    }
 
-				})
+                })
 
-				.batch(0) // avoid multiple truth-maintenance rounds
+                .filter(statement -> {
 
-				.forEach(new Upload()
-						.clear(true)
-						.contexts(EC2U.ontologies)
-				);
-	}
+                    final String lang=Values.lang(statement.getObject());
+
+                    return lang.isEmpty() || EC2U.Languages.contains(lang);
+
+                })
+
+                .batch(0) // avoid multiple truth-maintenance rounds
+
+                .forEach(new Upload()
+                        .clear(true)
+                        .contexts(EC2U.ontologies)
+                );
+    }
 
 }
