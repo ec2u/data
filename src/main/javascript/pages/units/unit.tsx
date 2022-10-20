636d45003897d6768be9403964b96a4b6727feaf
Alessandro Bollini <alessandro.bollini@metreeca.com>
2022-10-20 12:37:12 +0200

    Units / Add link to unit type from info box

--- src/main/javascript/pages/units/unit.tsx
+++ src/main/javascript/pages/units/unit.tsx
@@ -155,7 +155,7 @@ function DataEventInfo({
                 .map(unit => <li key={unit.id}><NodeLink>{unit}</NodeLink></li>)
             }</ul>,
 
-            "Type": classification && <span>{string(classification)}</span>
+            "Type": classification && <NodeLink>{classification}</NodeLink>
 
         }}</DataInfo>
 
