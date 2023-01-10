5b611208427eef03fee10d5a0a4d37d9cc74f9ca
Alessandro Bollini <alessandro.bollini@metreeca.com>
2023-01-10 10:11:59 +0100

    EventsTurkuTYY / Disabled after removal of JSON event catalog

--- src/main/java/eu/ec2u/data/Cron.java
+++ src/main/java/eu/ec2u/data/Cron.java
@@ -78,7 +78,7 @@ public final class Cron extends Delegator {
                 .path("/events/salamanca/city/to", execute(new EventsSalamancaCityTO()))
                 .path("/events/turku/university", execute(new EventsTurkuUniversity()))
                 // !!! .path("/events/turku/city", execute(new EventsTurkuCity()))
-                .path("/events/turku/tyy", execute(new EventsTurkuTYY()))
+                // !!! .path("/events/turku/tyy", execute(new EventsTurkuTYY()))
 
                 .path("/datasets/", execute(new Datasets.Updater()))
 
