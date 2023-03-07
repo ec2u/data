f85feee4c39d0d70f4db032a30b371f41ccf93e8
Alessandro Bollini <alessandro.bollini@metreeca.com>
2023-03-07 09:52:07 +0100

    Offers / Coimbra / Classify entries as programs/courses according to the `cicloTipo` field

--- src/main/java/eu/ec2u/data/offers/OffersCoimbra.java
+++ src/main/java/eu/ec2u/data/offers/OffersCoimbra.java
@@ -58,10 +58,12 @@ import static eu.ec2u.data.Data.exec;
 import static eu.ec2u.data.EC2U.University.Coimbra;
 import static eu.ec2u.data.EC2U.item;
 import static eu.ec2u.data.offers.Offers.Course;
+import static eu.ec2u.data.offers.Offers.Program;
 import static eu.ec2u.work.validation.Validators.validate;
 
 import static java.lang.String.format;
 import static java.util.Map.entry;
+import static java.util.function.Predicate.not;
 
 public final class OffersCoimbra implements Runnable {
 
@@ -98,7 +100,7 @@ public final class OffersCoimbra implements Runnable {
 
     private static final Pattern DurationPattern=Pattern.compile("(?<value>\\d+)\\s+(?<unit>\\w+)");
 
-    // ;( Java Duration natively supports only seconds
+    // ;( Java Duration natively supports either seconds-based durations or day-based periods
 
     private static final Map<String, Function<Integer, String>> ValueToDuration=Map.ofEntries(
             entry("year", OffersCoimbra::years),
@@ -167,10 +169,26 @@ public final class OffersCoimbra implements Runnable {
     @Override public void run() {
         Xtream.of(Instant.EPOCH)
 
-                .flatMap(this::courses)
-                .optMap(this::course)
+                .flatMap(this::offers)
+                .batch(0)
 
-                .pipe(courses -> validate(Course(), Set.of(Course), courses))
+                .flatMap(offers -> Xtream.from(
+
+                        Xtream.from(offers)
+
+                                .filter(OffersCoimbra::isProgram)
+                                .optMap(this::program)
+
+                                .pipe(programs -> validate(Program(), Set.of(Program), programs)),
+
+                        Xtream.from(offers)
+
+                                .filter(not(OffersCoimbra::isProgram))
+                                .optMap(this::course)
+
+                                .pipe(courses -> validate(Course(), Set.of(Course), courses))
+
+                ))
 
                 .forEach(new Upload()
                         .contexts(Context)
@@ -181,7 +199,12 @@ public final class OffersCoimbra implements Runnable {
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    private Xtream<JSONPath> courses(final Instant synced) {
+    private static boolean isProgram(final JSONPath entry) {
+        return entry.string("cicloTipo").filter("NAO_CONFERENTE_GRAU"::equals).isEmpty();
+    }
+
+
+    private Xtream<JSONPath> offers(final Instant synced) {
 
         final String url=vault
                 .get(APIUrl)
@@ -243,16 +266,16 @@ public final class OffersCoimbra implements Runnable {
                         return Optional.empty();
 
                     }
+
                 })
 
                 .map(JSONPath::new)
                 .flatMap(json -> json.paths("listaResultados.*"));
     }
 
-    private Optional<Frame> course(final JSONPath json) {
-        return json.integer("cursoId").map(id -> frame(item(Offers.Courses, Coimbra, String.valueOf(id)))
+    private Optional<Frame> offer(final IRI context, final JSONPath json) {
+        return json.integer("cursoId").map(id -> frame(item(context, Coimbra, String.valueOf(id)))
 
-                .values(RDF.TYPE, Course)
                 .value(Resources.university, Coimbra.Id)
 
                 .value(Schema.url, json.string("urlEN").map(Values::iri))
@@ -318,6 +341,22 @@ public final class OffersCoimbra implements Runnable {
                 .values(Schema.educationalCredentialAwarded, json.paths("qualificoesAtribuidas.*")
                         .optMap(this::localized)
                 )
+
+        );
+    }
+
+    private Optional<Frame> program(final JSONPath json) {
+        return offer(Offers.Programs, json).map(program -> program
+
+                .value(RDF.TYPE, Program)
+
+        );
+    }
+
+    private Optional<Frame> course(final JSONPath json) {
+        return offer(Offers.Courses, json).map(course -> course
+
+                .value(RDF.TYPE, Course)
 
         );
     }
