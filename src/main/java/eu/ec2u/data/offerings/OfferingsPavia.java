baced89ea06f894147d3311e7785f38ff12bdb03
Alessandro Bollini <alessandro.bollini@metreeca.com>
2024-10-30 15:02:45 +0100

    Offerings / Pavia / Integrate course details from UGov

--- src/main/java/eu/ec2u/data/offerings/OfferingsPavia.java
+++ src/main/java/eu/ec2u/data/offerings/OfferingsPavia.java
@@ -16,16 +16,20 @@
 
 package eu.ec2u.data.offerings;
 
-import com.metreeca.http.actions.Fill;
-import com.metreeca.http.actions.GET;
+import com.metreeca.http.actions.*;
 import com.metreeca.http.json.JSONPath;
 import com.metreeca.http.json.formats.JSON;
 import com.metreeca.http.rdf4j.actions.Upload;
+import com.metreeca.http.services.Logger;
+import com.metreeca.http.services.Vault;
 import com.metreeca.http.work.Xtream;
+import com.metreeca.http.xml.XPath;
+import com.metreeca.http.xml.formats.XML;
 import com.metreeca.link.Frame;
 
 import eu.ec2u.data.concepts.ISCED2011;
 import eu.ec2u.data.courses.Courses;
+import eu.ec2u.data.events.Events;
 import eu.ec2u.data.organizations.Organizations;
 import eu.ec2u.data.programs.Programs;
 import eu.ec2u.data.resources.Resources;
@@ -33,9 +37,16 @@ import eu.ec2u.data.things.Schema;
 import eu.ec2u.data.universities.University;
 import eu.ec2u.work.feeds.Parsers;
 import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Literal;
 import org.eclipse.rdf4j.model.vocabulary.ORG;
 import org.eclipse.rdf4j.model.vocabulary.SKOS;
+import org.eclipse.rdf4j.model.vocabulary.XSD;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Month;
@@ -45,20 +56,32 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+import static com.metreeca.http.Locator.service;
+import static com.metreeca.http.Request.POST;
+import static com.metreeca.http.Request.basic;
+import static com.metreeca.http.services.Logger.logger;
+import static com.metreeca.http.services.Vault.vault;
 import static com.metreeca.link.Frame.*;
 
 import static eu.ec2u.data.Data.exec;
 import static eu.ec2u.data.EC2U.item;
 import static eu.ec2u.data.EC2U.update;
+import static eu.ec2u.data.events.Events.EventAttendanceModeEnumeration.OfflineEventAttendanceMode;
 import static eu.ec2u.data.universities.University.Pavia;
+import static java.lang.String.format;
 import static java.util.Map.entry;
+import static java.util.function.Function.identity;
 import static java.util.function.Predicate.not;
 
 public final class OfferingsPavia implements Runnable {
 
     private static final IRI Context=iri(Offerings.Context, "/pavia");
 
-    private static final String APIUrl="https://studentionline.unipv.it/e3rest/api/offerta-service-v1/offerte/";
+    private static final String ESSE3Url="https://studentionline.unipv.it/e3rest/api/offerta-service-v1/offerte/";
+
+    private static final String APIUrl="offerings-pavia-url";
+    private static final String APIUsr="offerings-pavia-usr";
+    private static final String APIPwd="offerings-pavia-pwd";
 
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -93,6 +116,10 @@ public final class OfferingsPavia implements Runnable {
 
     );
 
+    private static final Map<String, Events.EventAttendanceModeEnumeration> DidToMode=Map.ofEntries(
+            entry("Convenzionale", OfflineEventAttendanceMode)
+    );
+
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -102,6 +129,10 @@ public final class OfferingsPavia implements Runnable {
 
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private final Vault vault=service(vault());
+    private final Logger logger=service(logger());
+
 
     @Override public void run() {
         update(connection -> Xtream.of(Instant.now())
@@ -115,12 +146,21 @@ public final class OfferingsPavia implements Runnable {
 
                 .flatMap(this::programs)
 
-                .flatMap(program -> Stream.concat(
+                .flatMap(program -> Stream.of(
 
                         program(program).stream(),
-                        courses(program).flatMap(course -> course(course).stream())
+                        details(program).flatMap(this::courses)
 
                 ))
+
+                .flatMap(identity())
+
+                // ;( deduplicate multiple courses w/ same afGenCod, retaining program to course links
+
+                .distinct(frame -> frame.id()
+                        .filter(iri -> frame.value(reverse(Programs.hasCourse)).isEmpty())
+                        .orElseGet(Frame::iri)
+                )
 
                 .flatMap(Frame::stream)
                 .batch(0)
@@ -134,7 +174,7 @@ public final class OfferingsPavia implements Runnable {
     }
 
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //// ESSE3 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     private Xtream<JSONPath> programs(final Year year) {
         return Xtream.of(0)
@@ -142,7 +182,7 @@ public final class OfferingsPavia implements Runnable {
                 .scan(start -> Xtream.of(start)
 
                         .flatMap(new Fill<>()
-                                .model(APIUrl+"?aaOffId={year}&start={start}&limit={limit}")
+                                .model(ESSE3Url+"?aaOffId={year}&start={start}&limit={limit}")
                                 .value("year", year)
                                 .value("start", start)
                                 .value("limit", Limit)
@@ -164,47 +204,12 @@ public final class OfferingsPavia implements Runnable {
                 );
     }
 
-    private Xtream<JSONPath> courses(final JSONPath program) {
-        return program.integers("aaOffId")
-
-                .flatMap(aaOffId -> program.integers("cdsOffId")
-
-                        .flatMap(cdsOffId -> Xtream.of(0)
-
-                                .scan(start -> Xtream.of(start)
-
-                                        .flatMap(new Fill<>()
-                                                .model(APIUrl+"{year}/{program}/attivita?start={start}&limit={limit}")
-                                                .value("year", aaOffId)
-                                                .value("program", cdsOffId)
-                                                .value("start", start)
-                                                .value("limit", Limit)
-                                        )
-
-                                        .optMap(new GET<>(new JSON()))
-
-                                        .map(JSONPath::new)
-                                        .map(json -> {
-
-                                            final List<JSONPath> list=json.paths("*").toList();
-
-                                            return entry(
-                                                    list.size() < Limit ? Stream.empty() : Stream.of(start+Limit),
-                                                    list.stream()
-                                            );
-
-                                        })
-                                )
-                        )
-                );
-    }
-
     private Optional<Frame> program(final JSONPath program) {
         return program.string("cdsCod").map(code -> frame(
 
-                // !!!  "logisticaExistsFlg": 1,
-                //      offertaExistsFlg": 1,
-                //      "statoAttCod": { "value": "A" },
+                // !!! "logisticaExistsFlg": 1,
+                // !!! "offertaExistsFlg": 1,
+                // !!! "statoAttCod": { "value": "A" },
 
                 field(ID, item(Programs.Context, Pavia, code)),
                 field(TYPE, Programs.EducationalOccupationalProgram),
@@ -235,65 +240,215 @@ public final class OfferingsPavia implements Runnable {
         ));
     }
 
-    private Optional<Frame> course(final JSONPath course) {
-        return course.string("chiaveAdContestualizzata.adCod").map(code -> {
 
-            final Optional<String> courseNameEN=course.string("adDesEng");
-            final Optional<String> courseNameIT=course.string("chiaveAdContestualizzata.adDes")
-                    .filter(it -> courseNameEN.filter(it::equals).isEmpty());
+    //// UGov //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            final Optional<String> programNameEN=course.string("cdsDesEng");
-            final Optional<String> programNameIT=course.string("chiaveAdContestualizzata.cdsDes")
-                    .filter(it -> programNameEN.filter(it::equals).isEmpty());
+    private Xtream<XPath> details(final JSONPath program) {
 
-            return frame(
+        final String url=vault
+                .get(APIUrl)
+                .orElseThrow(() -> new IllegalStateException(format(
+                        "undefined API URL <%s>", APIUrl
+                )));
 
-                    // !!! "adWebViewFlg": 1,
+        final String usr=vault
+                .get(APIUsr)
+                .orElseThrow(() -> new IllegalStateException(format(
+                        "undefined API user <%s>", APIUsr
+                )));
 
-                    field(ID, item(Courses.Context, Pavia, code)),
-                    field(TYPE, Courses.Course),
+        final String pwd=service(vault())
+                .get(APIPwd)
+                .orElseThrow(() -> new IllegalStateException(format(
+                        "undefined API password <%s>", APIPwd
+                )));
 
-                    field(Resources.university, Pavia.id),
+        return program.integers("aaOffId")
+                .flatMap(aaOffId -> program.strings("cdsCod")
+                        .flatMap(cdsCod -> Xtream.of(0)
 
-                    field(Courses.courseCode, literal(code)),
+                                .flatMap(new Fill<>()
+                                        .model(url)
+                                )
 
-                    field(Schema.name, courseNameEN.map(v -> literal(v, "en"))),
-                    field(Schema.name, courseNameIT.map(v -> literal(v, Pavia.language))),
+                                .optMap(new Query(request -> request
 
-                    field(Schema.inLanguage, course.strings("linguaInsDes")
-                            .flatMap(Parsers::languages)
-                            .map(Frame::literal)
-                    ),
+                                        .method(POST)
 
-                    field(Schema.url, course.string("urlSitoWeb")
-                            .flatMap(Parsers::url)
-                            .map(Frame::iri)
-                    ),
+                                        .header("Accept", XML.MIME)
+                                        .header("Authorization", basic(usr, pwd))
 
-                    field(Schema.url, course.string("urlCorsoMoodle")
-                            .flatMap(Parsers::url)
-                            .map(Frame::iri)
-                    ),
+                                        .body(new XML(), request(cdsCod, Year.of(aaOffId.intValue())))
 
-                    field(reverse(Programs.hasCourse), course.string("chiaveAdContestualizzata.cdsCod")
-                            .map(programCode -> frame(
+                                ))
 
-                                    field(ID, item(Programs.Context, Pavia, programCode)),
-                                    field(TYPE, Programs.EducationalOccupationalProgram),
+                                .optMap(new Fetch())
+                                .optMap(new Parse<>(new XML()))
+
+                                .map(XPath::new)
+                                .optMap(path -> path.path("//soap:Body/*"))
+
+                        )
+                );
+    }
+
+    private Document request(final String code, final Year year) {
+        try {
+
+            final String soap="http://schemas.xmlsoap.org/soap/envelope/";
+            final String ws="http://ws.di.u-gov.cineca.it/";
+
+
+            final DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+            final DocumentBuilder builder=factory.newDocumentBuilder();
+            final Document document=builder.newDocument();
+
+
+            final Element envelope=document.createElementNS(soap, "Envelope");
+
+            document.appendChild(envelope);
+
+            final Element header=document.createElementNS(soap, "Header");
+
+            envelope.appendChild(header);
+
+            final Element body=document.createElementNS(soap, "Body");
+
+            envelope.appendChild(body);
+
+            final Element programmazioneDidattica=document.createElementNS(ws, "ProgrammazioneDidattica");
+
+            body.appendChild(programmazioneDidattica);
+
+            final Element parametriEsportazioneProgrammazioneDidattica=document.createElementNS(null, "parametriEsportazioneProgrammazioneDidattica");
+
+            programmazioneDidattica.appendChild(parametriEsportazioneProgrammazioneDidattica);
+
+            final Element aaOffId=document.createElement("aaOffId");
+
+            aaOffId.setTextContent(year.toString());
+
+            parametriEsportazioneProgrammazioneDidattica.appendChild(aaOffId);
+
+            final Element cdsCod=document.createElement("cdsCod");
+
+            cdsCod.setTextContent(code);
+
+            parametriEsportazioneProgrammazioneDidattica.appendChild(cdsCod);
+
+
+            return document;
+
+        } catch ( final ParserConfigurationException e ) {
+
+            throw new RuntimeException("unable to create document builder", e);
+
+        }
+    }
+
+    private Xtream<Frame> courses(final XPath response) {
+
+        return response.paths("//ns2:cds").flatMap(cds -> {
+
+            final String program=cds.string("ns2:cdsCod").orElseThrow(() ->
+                    new IllegalArgumentException("missing cdsCod")
+            );
+
+            final String year=cds.string("ns2:aaOffId").orElseThrow(() ->
+                    new IllegalArgumentException("missing aaOffId")
+            );
+
+            return cds.paths("ns2:regdid[ns2:aaRegdidId='%s']/ns2:pds/ns2:af".formatted(year))
+                    .flatMap(af -> af.strings("ns2:afGenCod").flatMap(course -> Stream.of(
+
+                            frame(
+                                    field(ID, item(Courses.Context, Pavia, course)),
+                                    field(reverse(Programs.hasCourse), item(Programs.Context, Pavia, program))
+                            ),
+
+                            frame(
+
+                                    // !!! <ns2:inRegdidFlg>true</ns2:inRegdidFlg>
+                                    // !!! <ns2:nonErogabileFlg>false</ns2:nonErogabileFlg>
+
+                                    field(ID, item(Courses.Context, Pavia, course)),
+                                    field(TYPE, Courses.Course),
 
                                     field(Resources.university, Pavia.id),
 
-                                    field(Schema.identifier, literal(programCode)),
+                                    field(Courses.courseCode, literal(course)),
 
-                                    field(Schema.name, programNameEN.map(v -> literal(v, "en"))),
-                                    field(Schema.name, programNameIT.map(v -> literal(v, Pavia.language)))
+                                    field(Schema.name, af.string("ns2:afGenDes").map(v -> literal(v, Pavia.language))),
+                                    field(Schema.name, af.string("ns2:afGenDesEng").map(v -> literal(v, "en"))),
 
-                            ))
-                    )
+                                    // !!! <ns2:settCod>M-STO/04</ns2:settCod> to EuroSciVoc?
 
-            );
+                                    field(Courses.timeRequired, af.number("ns2:oreFrontAf")
+                                            .filter(v -> v > 0)
+                                            .map(OfferingsPavia::duration)
+                                    ),
+
+                                    field(Courses.courseWorkload, af.number("ns2:oreStuInd")
+                                            .filter(v -> v > 0)
+                                            .map(OfferingsPavia::duration)
+                                    ),
+
+                                    field(Offerings.teaches, text(af, "CONTENUTI")),
+                                    field(Offerings.assesses, text(af, "OBIETT_FORM")),
+                                    field(Courses.coursePrerequisites, text(af, "PREREQ")),
+
+                                    field(Schema.inLanguage, af.strings("ns2:linDid/ns2:linDidAf/ns2:linDidDes")
+                                            .flatMap(Parsers::languages)
+                                            .map(Frame::literal)
+                                    ),
+
+                                    field(Courses.courseMode, af.string("ns2:modDid/ns2:modDidAf/ns2:modDidDes")
+                                            .map(v -> {
+
+                                                final Events.EventAttendanceModeEnumeration mode=DidToMode.get(v);
+
+                                                if ( mode == null ) {
+                                                    logger.warning(this, format("unknown modDod <%s>", v));
+                                                }
+
+                                                return mode;
+
+                                            })
+                                    )
+
+                            )
+
+                    )));
 
         });
+
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private static Xtream<Literal> text(final XPath af, final String entry) {
+
+        // ;( multiple nsw:testi section are possible, eg for course partitions (<ns2:partStuCod>M-Z</ns2:partStuCod>)
+
+        return af.paths("ns2:testi[1]/ns2:testo[ns2:tipoTestoCod='%s']".formatted(entry))
+                .flatMap(testo -> testo.strings("ns2:contenuto")
+                        .flatMap(v -> testo.strings("ns2:linguaCod")
+                                .flatMap(Parsers::languages)
+                                .map(l -> literal(v, l))
+                        )
+                );
+    }
+
+    private static Literal duration(final double value) {
+
+        final int hours=Double.valueOf(value).intValue();
+        final int minutes=Double.valueOf(value%1*60).intValue();
+
+        return literal(
+                minutes == 0 ? format("PT%dH", hours) : format("PT%dH%dM", hours, minutes),
+                XSD.DURATION
+        );
     }
 
 }
--- src/main/java/eu/ec2u/data/offerings/OfferingsPavia.md
+++ src/main/java/eu/ec2u/data/offerings/OfferingsPavia.md
@@ -1,7 +1,13 @@
 # Integration
 
-* Data extracted from the
+* Basic data extracted from the
   public [ESSE3 API](https://studentionline.unipv.it/e3rest/docs/?urls.primaryName=Offerta%20Api%20V1%20(https%3A%2F%2Fstudentionline.unipv.it%2Fe3rest%2Fapi%2Fofferta-service-v1))
+* Extended data extracted from internal UGov APIs
+
+## 2024-10-31
+
+* UGov integration
+*
 
 ## 2024-10-30
 
@@ -9,15 +15,38 @@
 
 # Samples
 
-## Programs
+## Programs (ESSE3)
 
 ```http
 GET https://studentionline.unipv.it/e3rest/api/offerta-service-v1/offerte/?aaOffId=2024&start=0&limit=100
 ```
 
-## Courses
+## Courses (ESSE3)
 
 ```http
 GET https://studentionline.unipv.it/e3rest/api/offerta-service-v1/offerte/2024/10138/attivita?start=0&limit=100
+```
+
+## Course Details (UGov)
+
+```http
+POST {offerings-pavia-url}
+Accept: application/xml
+Authorization: Basic {offerings-pavia-usr}:{offerings-pavia-pwd}
+```
+
+```xml
+
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ws="http://ws.di.u-gov.cineca.it/">
+    <soapenv:Header/>
+    <soapenv:Body>
+        <ws:ProgrammazioneDidattica>
+            <parametriEsportazioneProgrammazioneDidattica>
+                <aaOffId>2023</aaOffId>
+                <cdsCod>32400</cdsCod>
+            </parametriEsportazioneProgrammazioneDidattica>
+        </ws:ProgrammazioneDidattica>
+    </soapenv:Body>
+</soapenv:Envelope>
 ```
 
