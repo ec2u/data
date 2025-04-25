032c946ef4ec1d01b017577925c2d3090def874d
Alessandro Bollini <alessandro.bollini@metreeca.com>
2025-04-25 15:58:42 +0200

    Taxonomies / SDGs / Refactor

--- src/main/java/eu/ec2u/data/taxonomies/SDGs.java
+++ src/main/java/eu/ec2u/data/taxonomies/SDGs.java
@@ -18,12 +18,8 @@ package eu.ec2u.data.taxonomies;
 
 import com.metreeca.flow.json.actions.Validate;
 import com.metreeca.flow.work.Xtream;
-import com.metreeca.mesh.meta.jsonld.Frame;
-import com.metreeca.mesh.meta.jsonld.Namespace;
 
-import eu.ec2u.data.organizations.OrgOrganization;
 import eu.ec2u.data.organizations.OrgOrganizationFrame;
-import eu.ec2u.data.resources.Reference;
 import eu.ec2u.data.resources.ReferenceFrame;
 import eu.ec2u.work.CSVProcessor;
 import org.apache.commons.csv.CSVRecord;
@@ -31,8 +27,6 @@ import org.apache.commons.csv.CSVRecord;
 import java.net.URI;
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.Locale;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -59,24 +53,57 @@ import static eu.ec2u.data.taxonomies.Taxonomies.TAXONOMIES;
  * description. This standardized terminology ensures consistent classification of resources related to sustainable
  * development across institutions within the EC2U alliance.
  */
-@Frame
-@Namespace("[dct]")
-public interface SDGs extends Taxonomy {
+public final class SDGs implements Runnable {
 
-    URI SDGS=TAXONOMIES.resolve("sdgs");
+    private static final URI SDGS=TAXONOMIES.resolve("sdgs");
 
-    OrgOrganizationFrame UNITED_NATIONS=new OrgOrganizationFrame()
+
+    private static final OrgOrganizationFrame UNITED_NATIONS=new OrgOrganizationFrame()
             .id(uri("http://un.org/"))
             .prefLabel(map(entry(EN, "United Nations")));
 
+    private static final TaxonomyFrame TAXONOMY=new TaxonomyFrame()
+            .id(SDGS)
+            .title(map(entry(EN, "United Nations Sustainable Development Goals")))
+            .alternative(map(entry(EN, "UN SDGs")))
+            .description(map(entry(EN,
+                    """
+                            [The 2030 Agenda for Sustainable Development](https://sdgs.un.org/2030agenda), adopted by all \
+                            United Nations Member States in 2015, provides a shared blueprint for peace and prosperity for \
+                            people and the planet, now and into the future. At its heart are the 17 Sustainable Development \
+                            Goals (SDGs), which are an urgent call for action by all countries - developed and developing - \
+                            in a global partnership. They recognize that ending poverty and other deprivations must go \
+                            hand-in-hand with strategies that improve health and education, reduce inequality, and spur \
+                            economic growth – all while tackling climate change and working to preserve our oceans \
+                            and forests."""
+            )))
+            .created(LocalDate.parse("2015-09-25"))
+            .issued(LocalDate.parse("2019-12-19"))
+            .rights("Copyright © 2019 United Nations")
+            .accessRights(map(entry(EN, """
+                    The SDG Knowledge Organization System, accessible at http://metadata.un.org/sdg, \
+                    is made available free of charge and may be copied freely, duplicated and further distributed \
+                    with the following citation: Amit Joshi, Luis Gonzalez Morales, Szymon Klarman, and Aaron Helton. \
+                    2019. A Knowledge Organization System for the United Nations Sustainable Development Goals. \
+                    New York, NY. http://metadata.un.org/sdg"""
+            )))
+            .publisher(UNITED_NATIONS)
+            .source(new ReferenceFrame()
+                    .id(uri("https://sdgs.un.org/goals")));
 
-    static void main(final String... args) {
-        exec(() -> service(store()).partition(SDGS).update(array(list(Xtream
+
+    public static void main(final String... args) {
+        exec(() -> new SDGs().run());
+    }
+
+
+    @Override public void run() {
+        service(store()).partition(SDGS).update(array(list(Xtream
 
                 .from(
 
                         Stream.of(
-                                new SDGsFrame(),
+                                TAXONOMY,
                                 UNITED_NATIONS
                         ),
 
@@ -87,86 +114,13 @@ public interface SDGs extends Taxonomy {
 
                 .optMap(new Validate<>())
 
-        )), FORCE));
+        )), FORCE);
     }
 
 
     //̸/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    @Override
-    default URI id() {
-        return SDGS;
-    }
-
-
-    @Override
-    default Map<Locale, String> title() {
-        return map(entry(EN, "United Nations Sustainable Development Goals"));
-    }
-
-    @Override
-    default Map<Locale, String> alternative() {
-        return map(entry(EN, "UN SDGs"));
-    }
-
-    @Override
-    default Map<Locale, String> description() {
-        return map(entry(EN,
-                """
-                        [The 2030 Agenda for Sustainable Development](https://sdgs.un.org/2030agenda), adopted by all \
-                        United Nations Member States in 2015, provides a shared blueprint for peace and prosperity for people and \
-                        the planet, now and into the future. At its heart are the 17 Sustainable Development Goals (SDGs), which \
-                        are an urgent call for action by all countries - developed and developing - in a global partnership. \
-                        They recognize that ending poverty and other deprivations must go hand-in-hand with strategies that \
-                        improve health and education, reduce inequality, and spur economic growth – all while tackling climate \
-                        change and working to preserve our oceans and forests."""
-        ));
-    }
-
-
-    @Override
-    default LocalDate created() {
-        return LocalDate.parse("2015-09-25");
-    }
-
-    @Override
-    default LocalDate issued() {
-        return LocalDate.parse("2019-12-19");
-    }
-
-
-    @Override
-    default String rights() {
-        return "Copyright © 2019 United Nations";
-    }
-
-    @Override
-    default Map<Locale, String> accessRights() {
-        return map(entry(EN, """
-                The SDG Knowledge Organization System, accessible at http://metadata.un.org/sdg, \
-                is made available free of charge and may be copied freely, duplicated and further distributed \
-                with the following citation: Amit Joshi, Luis Gonzalez Morales, Szymon Klarman, and Aaron Helton. \
-                2019. A Knowledge Organization System for the United Nations Sustainable Development Goals. \
-                New York, NY. http://metadata.un.org/sdg"""
-        ));
-    }
-
-
-    @Override
-    default OrgOrganization publisher() {
-        return UNITED_NATIONS;
-    }
-
-    @Override
-    default Reference source() {
-        return new ReferenceFrame()
-                .id(uri("https://sdgs.un.org/goals"));
-    }
-
-
-    //̸/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    final class Loader extends CSVProcessor<TopicFrame> {
+    private static final class Loader extends CSVProcessor<TopicFrame> {
 
         @Override protected Optional<TopicFrame> process(final CSVRecord record, final Collection<CSVRecord> records) {
             return value(record, "Goal Number").map(number -> new TopicFrame()
@@ -177,8 +131,8 @@ public interface SDGs extends Taxonomy {
                     .prefLabel(value(record, "Short Label").map(v -> map(entry(EN, v))).orElse(null))
                     .definition(value(record, "Extended Description").map(v -> map(entry(EN, v))).orElse(null))
 
-                    .inScheme(new SDGsFrame())
-                    .topConceptOf(new SDGsFrame()));
+                    .inScheme(TAXONOMY)
+                    .topConceptOf(TAXONOMY));
         }
 
     }
