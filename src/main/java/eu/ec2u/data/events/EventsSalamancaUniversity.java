6fa583e45f394f743cfcb39489b1de0ee838f0de
Alessandro Bollini <alessandro.bollini@metreeca.com>
2025-05-09 19:25:35 +0200

    Events / Salamanca / Integrate

--- src/main/java/eu/ec2u/data/events/EventsSalamancaUniversity.java
+++ src/main/java/eu/ec2u/data/events/EventsSalamancaUniversity.java
@@ -16,9 +16,47 @@
 
 package eu.ec2u.data.events;
 
+import com.metreeca.flow.http.actions.GET;
+import com.metreeca.flow.ical.formats.iCal;
+import com.metreeca.flow.services.Logger;
+import com.metreeca.flow.work.Xtream;
+import com.metreeca.mesh.tools.Store;
+
+import eu.ec2u.data.organizations.OrganizationFrame;
+import eu.ec2u.data.taxonomies.EC2UOrganizations;
+import net.fortuna.ical4j.model.component.VEvent;
+import net.fortuna.ical4j.model.property.Description;
+
+import static com.metreeca.flow.Locator.service;
+import static com.metreeca.flow.json.formats.JSON.store;
+import static com.metreeca.flow.services.Logger.logger;
+import static com.metreeca.flow.toolkits.Identifiers.AbsoluteIRIPattern;
+import static com.metreeca.mesh.Value.array;
+import static com.metreeca.mesh.util.Collections.*;
+import static com.metreeca.mesh.util.Loggers.time;
+import static com.metreeca.mesh.util.URIs.uri;
+
 import static eu.ec2u.data.Data.exec;
+import static eu.ec2u.data.resources.Localized.EN;
+import static eu.ec2u.data.universities.University.SALAMANCA;
+import static java.lang.String.format;
+import static java.util.Map.entry;
+import static net.fortuna.ical4j.model.Component.VEVENT;
 
 public final class EventsSalamancaUniversity implements Runnable {
+
+    private static final OrganizationFrame PUBLISHER=new OrganizationFrame()
+
+            .id(uri("https://eventum.usal.es/"))
+            .university(SALAMANCA)
+
+            .prefLabel(map(
+                    entry(EN, "University of Salamanca / Eventum"),
+                    entry(SALAMANCA.locale(), "Universidad de Salamanca / Eventum")
+            ))
+
+            .about(set(EC2UOrganizations.UNIVERSITY));
+
 
     public static void main(final String... args) {
         exec(() -> new EventsSalamancaUniversity().run());
@@ -27,7 +65,29 @@ public final class EventsSalamancaUniversity implements Runnable {
 
     //̸////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    @Override
-    public void run() { }
+    private final Store store=service(store());
+    private final Logger logger=service(logger());
+
+
+    @Override public void run() {
+        time(() -> store.insert(array(list(Xtream
+
+                .of("https://eventum.usal.es/ics/location/spain/lo-1.ics") // https://eventum.usal.es/kml.html
+
+                .optMap(new GET<>(new iCal()))
+
+                .flatMap(calendar -> calendar.getComponents(VEVENT).stream().map(VEvent.class::cast))
+
+                .optMap(v -> v.getDescription()
+                        .map(Description::getValue)
+                        .filter(AbsoluteIRIPattern.asMatchPredicate())
+                )
+
+                .pipe(new Events.Scanner(SALAMANCA, PUBLISHER))
+
+        )))).apply((elapsed, count) -> logger.info(EventsSalamancaUniversity.class, format(
+                "inserted <%,d> resources in <%,d> ms", count, elapsed
+        )));
+    }
 
 }
