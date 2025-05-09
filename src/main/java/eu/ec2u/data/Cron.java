b2c7d7f4dcb08105fadaa1ce6daa8dcac743417e
Alessandro Bollini <alessandro.bollini@metreeca.com>
2025-05-09 22:29:24 +0200

    Cron / Schedule Units/Linz

--- src/main/java/eu/ec2u/data/Cron.java
+++ src/main/java/eu/ec2u/data/Cron.java
@@ -48,6 +48,7 @@ public final class Cron extends Delegator {
                 .path("/units/coimbra", execute(new UnitsCoimbra()))
                 .path("/units/iasi", execute(new UnitsIasi()))
                 .path("/units/jena", execute(new UnitsJena()))
+                .path("/units/linz", execute(new UnitsLinz()))
                 .path("/units/pavia", execute(new UnitsPavia()))
                 .path("/units/poitiers", execute(new UnitsPoitiers()))
                 .path("/units/salamanca", execute(new UnitsSalamanca()))
